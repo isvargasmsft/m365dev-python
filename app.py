aa90a79a4ba0a9048a0b0e712dc7fbd948801eda
Isaac Vargas <isvargas@microsoft.com>
2023-08-10 08:39:28 -0600

    updates

--- app.py
+++ app.py
@@ -5,11 +5,15 @@ import asyncio
 
 import semantic_kernel as sk
 
-from azure.identity.aio import ClientSecretCredential
+from azure.identity.aio import EnvironmentCredential
 from kiota_authentication_azure.azure_identity_authentication_provider import AzureIdentityAuthenticationProvider
+from kiota_abstractions.api_error import APIError
 
 from msgraph import GraphRequestAdapter
 from msgraph import GraphServiceClient
+
+from msgraph.generated.models.todo_task_list import TodoTaskList
+from msgraph.generated.models.todo_task import TodoTask
 
 asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
@@ -17,7 +21,7 @@ kernel = sk.Kernel()
 
 # Create auth proviver object. Used to authenticate request
 
-credential = ClientSecretCredential("tenantID",
+credential = EnvironmentCredential("tenantID",
                                     "clientID",
                                     "clientSecret")
 scopes = ['https://graph.microsoft.com/.default']
@@ -33,7 +37,7 @@ client = GraphServiceClient(request_adapter)
 async def get_user_messages():
      """Getting the messages of a user"""
      try:
-         messages = await client.users_by_id("AlexW@contoso.com").messages.get()
+         messages = await client.users.by_user_id("AlexW@contoso.com").messages.get()
          for msg in messages.value:
              print(
                  msg.subject,
@@ -45,9 +49,6 @@ async def get_user_messages():
 asyncio.run(get_user_messages())
 
 # Create ToDo list
-from msgraph.generated.models.todo_task_list import TodoTaskList
-from msgraph.generated.models.todo_task import TodoTask
-
 my_list_id = "_"
 
 my_action_items = [
@@ -61,7 +62,7 @@ async def create_todo_list_and_tasks():
     try:
         request_body = TodoTaskList()
         request_body.display_name = 'Action items from emails'
-        result = await client.users_by_id("AlexW@M365x86781558.OnMicrosoft.com").todo.lists.post(request_body)
+        result = await client.users.by_user_id("AlexW@M365x86781558.OnMicrosoft.com").todo.lists.post(request_body)
         my_list_id = result.id
         print(my_list_id)
 
@@ -69,7 +70,7 @@ async def create_todo_list_and_tasks():
         for item in my_action_items:
             request_body = TodoTask()
             request_body.title = item
-            result = await client.users_by_id("AlexW@M365x86781558.OnMicrosoft.com").todo.lists_by_id(my_list_id).tasks.post(request_body)
+            result = await client.users.by_user_id("AlexW@M365x86781558.OnMicrosoft.com").todo.lists.by_list_id(my_list_id).tasks.post(request_body)
 
     except Exception as e_rr:
         print(f'Error: {e_rr.error.message}')
