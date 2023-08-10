89b8e49280683720af2879702533bf8f4f8d61e8
Isaac Vargas <isvargas@microsoft.com>
2023-08-10 08:29:55 -0600

    updates

--- app.py
+++ app.py
@@ -46,40 +46,32 @@ asyncio.run(get_user_messages())
 
 # Create ToDo list
 from msgraph.generated.models.todo_task_list import TodoTaskList
+from msgraph.generated.models.todo_task import TodoTask
 
-# variable to store the ID of the new list
 my_list_id = "_"
 
-async def create_todo_list():
-    """Create a ToDo list"""
-    try:
-        request_body = TodoTaskList()
-        request_body.display_name = 'Action items from emails'
-        result = await client.users_by_id("AlexW@contoso.com").todo.lists.post(request_body)
-        my_list_id = result.id
-        print(my_list_id)
-    except Exception as e_rr:
-        print(f'Error: {e_rr.error.message}')
-
-asyncio.run(create_todo_list())
-
-# Create new tasks
-from msgraph.generated.models.todo_task import TodoTask
-async def create_new_task(item):
-    """Create a new task"""
-    try:
-        request_body = TodoTask()
-        request_body.title = item
-        result = await client.users_by_id("AlexW@contoso.com").todo.lists_by_id(my_list_id).tasks.post(request_body)
-    except Exception as e_rr:
-        print(f'Error: {e_rr.error.message}')
-
-# Placeholder of actions items. Real action items will be extracted from emails using semantic kernel.
 my_action_items = [
     "Create email", 
     "Review presentation",
     "Schedule meeting"
 ]
 
-for item in my_action_items:
-    asyncio.run(create_new_task(item))
+async def create_todo_list_and_tasks():
+    """Create a ToDo list"""
+    try:
+        request_body = TodoTaskList()
+        request_body.display_name = 'Action items from emails'
+        result = await client.users_by_id("AlexW@M365x86781558.OnMicrosoft.com").todo.lists.post(request_body)
+        my_list_id = result.id
+        print(my_list_id)
+
+        # Adding tasks
+        for item in my_action_items:
+            request_body = TodoTask()
+            request_body.title = item
+            result = await client.users_by_id("AlexW@M365x86781558.OnMicrosoft.com").todo.lists_by_id(my_list_id).tasks.post(request_body)
+
+    except Exception as e_rr:
+        print(f'Error: {e_rr.error.message}')
+
+asyncio.run(create_todo_list_and_tasks())
